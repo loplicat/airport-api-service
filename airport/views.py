908e8701bed6dd78f865dd00eb32e65ee8bb78b0
Lana Chernenko <svitlaana.chernenko.s@gmail.com>
2024-06-05 18:50:19 +0100

    Create views

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from airport.models import (
+    Country,
+    City,
+    AirplaneType,
+    Airplane,
+    Airport,
+    Crew,
+    Route, Flight,
+)
+from airport.serializers import (
+    CountrySerializer,
+    CitySerializer,
+    AirplaneTypeSerializer,
+    AirplaneSerializer,
+    AirportSerializer,
+    CrewSerializer,
+    RouteSerializer, FlightSerializer,
+)
+
+
+class CountryViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class CityViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class AirplaneTypeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class AirportViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class CrewViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class RouteViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class FlightViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+
