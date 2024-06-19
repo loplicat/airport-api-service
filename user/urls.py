5bbf2f0ad865ebe7c9f4a1738a40843adeebbd6c
Lana Chernenko <svitlaana.chernenko.s@gmail.com>
2024-06-19 14:47:14 +0100

    Implement login and creating user through the email

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from user.views import CreateUserView, LoginUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", LoginUserView.as_view(), name="login"),
+    path("login/",  LoginUserView.as_view(), name="token"),
     path("me/", ManageUserView.as_view(), name="manage")
 ]
 
